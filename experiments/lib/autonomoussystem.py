8f21cd0bb3ceee31655500b7996bc04330934586
Dexter Tan <dev@dextertanyj.com>
2023-03-01 15:05:58 +0800

    Update experiments

---
+++ experiments/lib/autonomoussystem.py
@@ -0,0 +1,88 @@
+from dataclasses import dataclass
+from typing import List, Tuple
+from . import router
+from mininet.topo import Topo
+
+
+@dataclass
+class RouterInfo:
+    name: str
+    lo_ip: str
+    core_ip: str
+    host_subnet_prefix: str
+    local_switch: str
+
+    def __post_init__(self) -> None:
+        self.interface_count = 0
+
+    def getInterface(self) -> str:
+        name = f'{self.name}-eth{self.interface_count}'
+        self.interface_count += 1
+        return name
+
+
+@dataclass
+class HostInfo:
+    name: str
+    ip: str
+    gateway: RouterInfo
+
+
+class AutonomousSystem:
+    as_count = 0
+
+    @staticmethod
+    def getASPrefix(asn: int) -> int:
+        return asn // 100
+
+    @classmethod
+    def getASN(cls) -> int:
+        assert cls.as_count < 255, 'Insufficient resources'
+        cls.as_count += 1
+        return cls.as_count * 100
+
+    def __init__(self, router_count: int, hosts_per_router: int, router_type: type[router.BGPRouter]) -> None:
+        assert router_count < 10, 'Unsupported AS size'
+        assert hosts_per_router < 10, 'Unsupported AS size'
+        self.asn = AutonomousSystem.getASN()
+        self.router_type: type[router.BGPRouter] = router_type
+        self.routers: List[RouterInfo] = []
+        self.switches: List[str] = []
+        self.links: List[Tuple[str, str]] = []
+        self.hosts: List[HostInfo] = []
+        self.switches.append(f's{self.asn}')  # AS core switch
+        for idx in range(1, router_count + 1):
+            as_prefix = AutonomousSystem.getASPrefix(self.asn)
+            router_name = f'r{self.asn + idx * 10}'
+            host_subnet_prefix = f'10.{as_prefix}.{idx}'
+            lo_ip = f'127.0.{as_prefix}.{idx}'
+            core_ip = f'192.168.{as_prefix}.{idx}'
+            switch_name = f's{self.asn + idx * 10}'
+            router_info = RouterInfo(router_name, lo_ip, core_ip, host_subnet_prefix, switch_name)
+            self.routers.append(router_info)
+            self.switches.append(switch_name)  # Local switch to hosts
+            self.links.append([router_name, f'S{self.asn}'])  # Link to core switch
+            self.links.append([router_name, switch_name])  # Link to local switch
+            for host in range(1, hosts_per_router + 1):
+                host_name = f'h{as_prefix}{idx}{host}'
+                host_ip = f'{router_info.host_subnet_prefix}.{host}'
+                self.hosts.append(HostInfo(host_name, host_ip, router_info))
+                self.links.append([host_name, switch_name])
+
+    def construct(self, topology: Topo):
+        for switch in self.switches:
+            topology.addSwitch(switch)
+        for router in self.routers:
+            topology.addNode(router.name, cls=self.router_type, ip=None,
+                             lo=router.lo_ip)
+            topology.addLink(router.name, router.local_switch, intfName1=router.getInterface(),
+                             params1={'ip': f'{router.host_subnet_prefix}.254/24'})
+            topology.addLink(router.name, self.switches[0], intfName1=router.getInterface(),
+                             params1={'ip': f'{router.core_ip}/24'})
+        for host in self.hosts:
+            topology.addHost(host.name, ip=f'{host.ip}/24', defaultRoute=f'via {host.gateway.host_subnet_prefix}.254')
+            topology.addLink(host.name, host.gateway.local_switch)
+
+    def getRouter(self, router_id: int):
+        assert router_id > 0 and router_id <= len(self.routers)
+        return self.routers[router_id - 1]
---
+++ experiments/simple-frr/topology.py
@@ -0,0 +1,85 @@
+import atexit
+import os
+import sys
+
+from mininet.cli import CLI
+from mininet.link import Link
+from mininet.log import info, setLogLevel
+from mininet.net import Mininet
+from mininet.topo import Topo
+
+CWD = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(CWD, '../'))
+
+from lib import autonomoussystem, router  # NOQA
+
+net = None
+
+
+class BasicTopo(Topo):
+    """
+    AS 1 contains 2 hosts.
+    AS 2 contains 1 host.
+    AS 3 contains 3 hosts.
+    AS 4 contains 2 hosts.
+
+    AS 1 provides AS 2.
+    AS 2 provides AS 4 and peers with AS 3.
+    """
+
+    def build(self):
+        # AS 1
+        as_one = autonomoussystem.AutonomousSystem(1, 2, router.FRRRouter)
+        as_one.construct(self)
+        # AS 2
+        as_two = autonomoussystem.AutonomousSystem(1, 1, router.FRRRouter)
+        as_two.construct(self)
+        # AS 3
+        as_three = autonomoussystem.AutonomousSystem(1, 3, router.FRRRouter)
+        as_three.construct(self)
+        # AS 4
+        as_four = autonomoussystem.AutonomousSystem(1, 2, router.FRRRouter)
+        as_four.construct(self)
+
+        # BGP peering links
+        as_one_router_one = as_one.getRouter(1)
+        as_two_router_one = as_two.getRouter(1)
+        as_three_router_one = as_three.getRouter(1)
+        as_four_router_one = as_four.getRouter(1)
+
+        self.addLink(as_one_router_one.name, as_two_router_one.name,
+                     intfName1=as_one_router_one.getInterface(), params1={'ip': '172.17.1.1/30'},
+                     intfName2=as_two_router_one.getInterface(), params2={'ip': '172.17.1.2/30'})
+        self.addLink(as_two_router_one.name, as_three_router_one.name,
+                     intfName1=as_two_router_one.getInterface(), params1={'ip': '172.18.1.1/30'},
+                     intfName2=as_three_router_one.getInterface(), params2={'ip': '172.18.1.2/30'})
+        self.addLink(as_two_router_one.name, as_four_router_one.name,
+                     intfName1=as_two_router_one.getInterface(), params1={'ip': '172.18.2.1/30'},
+                     intfName2=as_four_router_one.getInterface(), params2={'ip': '172.18.2.2/30'})
+
+
+def startNetwork():
+    info('*** Creating the network\n')
+    topology = BasicTopo()
+
+    global net
+    net = Mininet(topo=topology, link=Link, autoSetMacs=True)
+
+    info('*** Starting the network\n')
+    net.start()
+    info('*** Running CLI\n')
+    CLI(net)
+
+
+def stopNetwork():
+    if net is not None:
+        net.stop()
+
+
+if __name__ == '__main__':
+    # Force cleanup on exit by registering a cleanup function
+    atexit.register(stopNetwork)
+
+    # Tell mininet to print useful information
+    setLogLevel('info')
+    startNetwork()
